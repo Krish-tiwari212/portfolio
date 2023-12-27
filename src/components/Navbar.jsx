e77d639635422a9c42491960e09875250e1168dc
krish <championcoc.212@gmail.com>
2023-12-28 01:38:43 +0530

    updated responsiveness for section 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { TbMenu } from "react-icons/tb";
 import { RxCross1 } from "react-icons/rx";
 
@@ -6,14 +6,8 @@ const Navbar = () => {
   const navItems = ['Resume', 'About', 'LinkedIn', 'Contact'];
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    // Calculate the viewport height and set it as a CSS variable
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, []);
-
   return (
-    <div className='z-20 sm:ml-[5rem] sticky top-0 h-[calc(var(--vh, 5vh) * 200)]'>
+    <div className='z-20 sm:ml-[5rem] sticky top-0'>
       <nav className='w-[100vw] -mt-[1.3rem] pb-4 sm:-ml-[5rem] flex justify-between backdrop-blur-lg bg-opacity-80'>
         <h3 className='sm:ml-[4rem] ml-6 mt-[1.3rem]'>Full Stack Developer <span className='text-xl tracking-[-0.1em]'>---</span> Krish</h3>
         <button className='sm:hidden pr-5' onClick={() => setIsOpen(!isOpen)}>
