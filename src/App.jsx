f61f50585eb9283651f7a5ab5f6841a649316d82
krish <championcoc.212@gmail.com>
2023-12-28 19:10:18 +0530

    added lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 // App.js
-import React from 'react';
-import './index.css';
+import React, { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { FaArrowTurnDown } from 'react-icons/fa6';
+import Navbar from './components/Navbar';
 import Section1 from './components/sections/Section1';
 import Section2 from './components/sections/Section2';
 import Section3 from './components/sections/Section3';
 import TableSkeleton from './components/TableSkeleton';
-import { FaArrowTurnDown } from "react-icons/fa6";
-import Navbar from './components/Navbar';
+import './index.css'; // Import your global styles
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenis.on('scroll', (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className='bg-[#060606] font-geist-regular text-[#e6e6e6]'>
       <Navbar />
@@ -16,13 +35,13 @@ const App = () => {
       <Section2 />
       <Section3 />
       <section className='mx-20 relative'>
-        <h2 className='flex text-4xl mb-6 mt-4 font-geist-light'><FaArrowTurnDown className="mr-2 mt-2 font-light text-3xl"/> About Me </h2>
+        <h2 className='flex text-4xl mb-6 mt-4 font-geist-light'>
+          <FaArrowTurnDown className='mr-2 mt-2 font-light text-3xl' /> About Me
+        </h2>
         <hr />
-        <TableSkeleton numCoulmns={9} numRows={1} cl="" cd= "absolute -mt-7"/>
+        <TableSkeleton numCoulmns={9} numRows={1} cl='' cd='absolute -mt-7' />
         <div className='flex'>
-          <h2>
-
-          </h2>
+          <h2>{/* Your content goes here */}</h2>
         </div>
       </section>
     </div>
--- src/components/StickyScroll.jsx
+++ src/components/StickyScroll.jsx
@@ -41,7 +41,7 @@ const StickyScroll = () => {
   });
   return (
     
-        <div className='cards pb-[50vh] sm:pb-[47vh] md:pb-[20vh] lg:pb-[83vh]' ref={cardContainerRef}>
+        <div className='cards pb-[50vh] sm:pb-[47vh] md:pb-[60vh] lg:pb-[83vh]' ref={cardContainerRef}>
             <ProjectCard color="bg-[#F2AFEF]" cl="card" name="Vitrate" year="2023" image={vitrate} techstack="Flask, Postgresql, Postman, Algolia, Js"/>
             <ProjectCard color="bg-[#F1E4C3]" cl="card" name="Raccoon" year="2023" image={raccoon} techstack="Flask, Snipcart, Postgresql, Postman, Js"/>
             <ProjectCard color="bg-[#C6A969]" cl="card" name="Groco" year="2023" image={groco} techstack="Flask, Postgresql, Postman, Snipcart, Js"/>
