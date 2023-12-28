25a250abd3c90accccfa15e5af5b9dcf2b3a1d6c
krish <championcoc.212@gmail.com>
2023-12-28 17:57:22 +0530

    fixed whitespace bug

--- src/components/sections/Section1.jsx
+++ src/components/sections/Section1.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
 import TableSkeleton from '../TableSkeleton';
 import ConstrainedMovementAnimation from '../ConstrainedMovementAnimation';
@@ -6,10 +6,21 @@ import Marquee from '../Marquee';
 import "../../index.css";
 
 const Section1 = () => {
+  const [numColumns, setNumColumns] = useState(17);
+  const [numRows, setNumRows] = useState(16);
+
+  useEffect(() => {
+    // Adjust the number of columns and rows based on the screen width
+    if (window.innerWidth <= 768) { // 768px is typically the width for mobile devices
+      setNumColumns(10);
+      setNumRows(20);
+    }
+  }, []);
+
   return (
     <section className='h-[100dvh] px-2 sm:px-20 pt-4 -mt-4 cont relative border-[#6b6b6b] border-b-[1px]'>
         <div>
-          <TableSkeleton numCoulmns={17} numRows={16}/>
+          <TableSkeleton numCoulmns={numColumns} numRows={numRows}/>
           <ConstrainedMovementAnimation />
           <Marquee />
         </div>
