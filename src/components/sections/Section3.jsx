1297b83386ea722bd66001e25651f8c46eebb47d
krish <championcoc.212@gmail.com>
2023-12-28 18:53:23 +0530

    few more issue fixes

--- src/components/sections/Section3.jsx
+++ src/components/sections/Section3.jsx
@@ -6,9 +6,9 @@ import StickyScroll from '../StickyScroll';
 const Section3 = () => {
   return (
     <section className='mx-2 sm:mx-20 relative'>
-        <h2 className='flex text-4xl mb-7 font-geist-light'><FaArrowTurnDown className="mr-2 mt-2 font-light text-3xl"/> Projects </h2>
+        <h2 className='flex text-4xl mb-6 font-geist-light'><FaArrowTurnDown className="mr-2 mt-2 font-light text-3xl"/> Projects </h2>
         <hr />
-        <TableSkeleton numCoulmns={9} numRows={1} cl="h-[260vh] lg:h-[510vh] md:h-[366vh] sm:h-[440vh]" cd= "absolute -mt-7"/>
+        <TableSkeleton numCoulmns={9} numRows={1} cl="h-[305vh] lg:h-[510vh] md:h-[366vh] sm:h-[320vh]" cd= "absolute -mt-7"/>
         <StickyScroll />
     </section>
   )
