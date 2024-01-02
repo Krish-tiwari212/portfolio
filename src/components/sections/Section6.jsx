104baf6d54105ffeebbfde188d0c498a84a9ee63
krish <championcoc.212@gmail.com>
2024-01-02 12:51:15 +0530

    contact fix

--- src/components/sections/Section6.jsx
+++ src/components/sections/Section6.jsx
@@ -22,14 +22,14 @@ const Section6 = () => {
           <FaArrowTurnDown className='mt-3 font-geist-light text-[3vh]' /> Contact
         </h2>
         <hr className='-mt-1'/>
-        <TableSkeleton numCoulmns={numColumns} numRows={numRows} cl='' cd='absolute -mt-7 h-[10rem]' />
+        <TableSkeleton numCoulmns={numColumns} numRows={numRows} cl='' cd='absolute h-[38rem]  -mt-7 sm:h-[10rem]' />
         <div className='h-[40rem] relative'>
             <div className='absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center -mt-10'>
                 <BlurredCircle />
             </div>
             <p className='absolute text-[#707070] top-0 right-0 bottom-0 left-0 flex items-center justify-center -mt-[12rem]'>Say Hello</p>
             <h2>
-                <a href="mailto:krishtiwari2122@gmail.com" className='text-[2rem] md:text-[3rem] absolute top-0 right-0 bottom-0 -mt-[6rem] left-0 flex items-center justify-center'>krishtiwari2122@gmail.com</a>
+                <a href="mailto:krishtiwari2122@gmail.com" className='text-[7vw] md:text-[3rem] absolute top-0 right-0 bottom-0 -mt-[6rem] left-0 flex items-center justify-center'>krishtiwari2122@gmail.com</a>
             </h2>
             <div className='bottom-0 absolute right-0 left-3 mb-8'>
                 <h2>Made with ❤️ by Krish</h2>
