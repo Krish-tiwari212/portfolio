300ca5e84cad27cbd33dcdf128206d2f7a4776dc
krish <championcoc.212@gmail.com>
2024-09-17 17:02:47 +0530

    changed dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import Navbar from './components/Navbar';
 import Section1 from './components/sections/Section1';
 import Section2 from './components/sections/Section2';
